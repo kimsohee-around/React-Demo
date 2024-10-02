3aa6c239f01295143fbec50412858745fef9cd62
soheekim <koreait.sec2020@gmail.com>
2024-10-02 15:02:32 +0900

    #8 예약 날짜 UI 제어하기2 reducer 의 액션을 모두 테스트 완료

--- src/component/Bookings/WeekPicker.jsx
+++ src/component/Bookings/WeekPicker.jsx
@@ -15,6 +15,24 @@ function WeekPicker({date}){
                     <FaChevronLeft/>
                     <span>PREV</span>
                 </button>
+                <input type="date" defaultValue={formatDate(new Date())}
+                       onChange={(e) => dispatch({
+                           type: "SET_DATE",
+                           payload: e.target.value
+                       })}
+                />
+                {/* 입력값을 state(상태값)으로 관리하면....
+                    이 예제에서는 날짜 타입으로 변환되는 문자열이 아니면
+                    예상치 않은 결과로 보입니다.
+                    입력값을 state 관리는 기능에 맞는 경우에만 사용합니다.
+                */}
+                <input type="text" placeholder="yyyy-mm-dd"
+                       defaultValue={formatDate(new Date())}
+                       onChange={(e) => dispatch({
+                           type: "SET_DATE",
+                           payload: e.target.value
+                       })}
+                />
                 <button
                     className="btn"
                     onClick={() => dispatch({type: "TODAY"})}>
@@ -29,12 +47,23 @@ function WeekPicker({date}){
                 </button>
             </p>
             <p>
-                {week.start.toLocaleString()} ~ {week.end.toLocaleString()}
+                {/*{week.start.toLocaleString().substring(0,13)}
+                ~ {week.end.toLocaleString().substring(0,13)}*/}
+                {formatDate(week.start)} ~ {formatDate(week.end)}
             </p>
         </div>
 
     )
 
 }
+
+function formatDate(date) {
+    const year = date.getFullYear()
+    const month = String(date.getMonth()+1).padStart(2,'0')
+    const day = String (date.getDate()).padStart(2,'0')
+
+    return [year,month,day].join('-')
+}
+
 
 export default WeekPicker
