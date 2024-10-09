6a6fcb0e07ac665c33c130932cb7ce8805685218
kimsohee-around <koreait.sec2020@gmail.com>
2024-10-09 15:53:10 +0900

    useFetch bookable 커스텀 훅 적용

---
+++ src/component/utils/useFetch.js
@@ -0,0 +1,34 @@
+import {useEffect, useState} from "react";
+import loadData from "./api.js";
+
+export default function useFetch(url){
+    const [data, setData] = useState()
+    const [error, setError] = useState(null)
+    const [status, setStatus] = useState("idle")
+
+    useEffect(() => {
+        // 여러 컴포넌트와 공유하게 될 코드로 실행할 때마다 변수 reset
+        let doUpdate = true
+        setStatus("loading")
+        setData(undefined)
+        setError(null)
+
+        loadData(url)
+            .then(data =>{
+                if(doUpdate){
+                    setData(data)
+                    setStatus("success")
+                }
+            })
+            .catch(error => {
+                if(doUpdate){
+                    setError(error)
+                    setStatus("error")
+                }
+            })
+
+        return () => doUpdate=false
+    }, []);
+
+    return {data,status,error}
+}
