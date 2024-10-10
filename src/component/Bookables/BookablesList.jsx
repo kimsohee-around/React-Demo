6514d6b17983fdeb38b817c446185eb544cf63db
soheekim <koreait.sec2020@gmail.com>
2024-10-10 10:42:46 +0900

    #23 custom hook BookablesList 리팩토링

--- src/component/Bookables/BookablesList.jsx
+++ src/component/Bookables/BookablesList.jsx
@@ -2,34 +2,37 @@ import { useEffect, useState} from "react";
 import {FaArrowRight} from "react-icons/fa";
 import Spinner from "../UI/Spinner.jsx";
 import loadData from "../utils/api.js";
+import useFetch from "../utils/useFetch.js";
 
 // bookables 는 전체 목록, bookable 은 목록 중에 선택한 하나의 객체를 컴포넌트 프롭으로 받음.
 //자식 컴포넌트에서 부모컴포넌트가 전달한 state 변수를 props 로 받음.
 export default function BookablesList ({bookable, setBookable}) {
-    //자식 BookablesList 컴포넌트가 관리하는 state 변수 선언
-    //예약 가능 자원 데이터를 fetch 수신하는데 필요한 상태 변수
-    const [bookables, setBookables] = useState([]);
-    const [error, setError] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-
+//  "http://localhost:3001/bookables"
+//  커스텀 훅 호출. useFetch 리턴받은 객체를 구조 분해하여 저장.
+//  data 프로퍼티는 변수명 bookables 로 저장.data 값이 없으면 [] 배열로 초기화
+    const {data:bookables=[],status,error} = useFetch(
+        "http://localhost:3001/bookables"
+    )
+    // 현재 컴포넌트의 bookable 인자는 select 로 선택하는 그룹값을 포함.
     const group = bookable?.group;
     const bookablesInGroup = bookables.filter(b => b.group === group);
+    // bookables 를 fetch 한 후에 새로운 그룹이 있을 경우 실행 필요.
     const groups = [...new Set(bookables.map(b => b.group))];
 
+    // 데이터를 fetch 한 후에 설정되는 초기화
     useEffect(() => {
-        loadData("http://localhost:3001/bookables")
-            .then(bookables => {
-                setBookable(bookables[0]);
-      //fetch 결과 bookable 을 변경 -> 프롭으로 받은 상태변수로
-                //변경결과 부모 컴포넌트에게 상향
-                setBookables(bookables);
-                setIsLoading(false);
-            })
-            .catch(error => {
-                setError(error);
-                setIsLoading(false);
-            });
-    }, [setBookable]);
+        setBookable(bookables[0])
+    }, [bookables,setBookable]);
+    //의존성 배열은 useEffect 안에서 사용한 변수, 함수 모두 포함하는 것이 규칙
+
+    if (status === "error"){
+        return <p>{error.message}</p>
+    }
+
+    if (status === "loading") {
+        return <p><Spinner/>Loading bookables....</p>
+    }
+
 
     function changeGroup (e) {
         const bookablesInSelectedGroup = bookables.filter(
@@ -43,14 +46,6 @@ export default function BookablesList ({bookable, setBookable}) {
         const nextIndex = (i + 1) % bookablesInGroup.length;
         const nextBookable = bookablesInGroup[nextIndex];
         setBookable(nextBookable);
-    }
-
-    if (error) {
-        return <p>{error.message}</p>
-    }
-
-    if (isLoading) {
-        return <p><Spinner/> Loading bookables...</p>
     }
 
     return (
